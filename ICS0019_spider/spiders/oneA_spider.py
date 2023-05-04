5416bb514368737d8933d592ce558f9fb981619e
Pavel Rotov <paroto@ttu.ee>
2023-05-04 16:04:54 +0300

    oneA_spider: renamed + now parses whole 1a.ee  website

--- ICS0019_spider/spiders/oneA_spider.py
+++ ICS0019_spider/spiders/oneA_spider.py
@@ -5,19 +5,29 @@ import os
 class LaptopCrawl(scrapy.Spider):
     name = "laptop"
 
-    home_url = 'https://www.1a.ee'
-
     start_urls = [
-        'https://www.1a.ee/c/arvutitehnika-burootarbed/sulearvutid-ja-tarvikud/sulearvutid/373'
+        'https://www.1a.ee'
     ]
 
     def parse(self, response, **kwargs):
+        for category_url in response.css("li.submenu-lvl2__list-item a.submenu-lvl2__list-item-link::attr(href)").getall():
+            category_url = response.urljoin(category_url)
+            yield scrapy.Request(category_url, callback=self.parse_pages)
+
+
+
+    def parse_pages(self, response):
         for block in response.css(
                 "div.catalog-taxons-products-container div.catalog-taxons-products-container__grid-row div.catalog-taxons-product.catalog-taxons-product--grid-view"):
             page = scrapy.Request(
                 os.path.join('https://www.1a.ee', block.css("a.catalog-taxons-product__name::attr(href)").get()[1:]),
                 callback=self.parse_product)
             yield page
+
+        next_page = response.css("a.paginator__next::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_pages)
 
     def parse_product(self, response):
         def return_specifications(response):
